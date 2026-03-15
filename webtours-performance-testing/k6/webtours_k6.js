1b24fabd923b88e2e1077002ba3e960db4f12da3
shatov <shatov@playkot.com>
2026-03-15 23:48:27 +0300

    test

--- webtours-performance-testing/k6/webtours_k6.js
+++ webtours-performance-testing/k6/webtours_k6.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-
+// test
 const WT_HOST = __ENV.WT_HOST || "http://webtours.load-test.ru:1080";
 const WT_WEB = `${WT_HOST}/webtours`;
 const WT_CGI = `${WT_HOST}/cgi-bin`;
